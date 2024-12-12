431f37e4bfd785a88f22559bff71b754ba1c0141
Lucas <lucas.lebars@edu.univ-fcomte.fr>
2024-12-12 11:50:23 +0100

    page info presque terminée

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,19 @@
 const toggle = document.querySelector(".boutonmenu");
+const nav = document.querySelector("#menu");
+const accueil = document.getElementById("accueil");
+
+if (toggle && nav) {
+  toggle.addEventListener("click", () => {
+    const isOpen = toggle.ariaExpanded === "true";
+    const isClosed = !isOpen;
+
+    toggle.ariaExpanded = isClosed;
+    nav.hidden = isOpen; 
+
+    if (isClosed) {
+      accueil.style.display = "none"; 
+    } else {
+      accueil.style.display = "block"; 
+    }
+  });
+}
